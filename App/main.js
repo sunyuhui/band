a0bacf2d42fe46ff40e2e8726ea8dd35c2ffc957
sunhui04 <sunhui04@meituan.com>
2017-10-11 17:27:29 +0800

    init

---
+++ App/main.js
@@ -0,0 +1,23 @@
+import koa from 'koa';
+
+class APP {
+	constructor(props) {
+		this.app = new koa();
+	},
+	init() {
+
+	},
+	start(port) {
+		let app = this.app;
+		app.use((ctx)=>{
+			ctx.body = 'sunyuhui';
+		})
+		app.listen(port);
+	}
+}
+
+const project = new APP();
+console.log('project测试', project)
+
+// export default project;
+module.exports = project;
---
+++ project/main.js
@@ -0,0 +1,5 @@
+// import project from '../App/main.js';
+var project = require('../App/main')
+
+project.start(3000);
+console.log('listen on 3000...');
