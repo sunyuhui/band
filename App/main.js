1a39fbe6bd675fbc16b645062d124b650bcdef66
sunhui04 <sunhui04@meituan.com>
2017-10-14 13:15:53 +0800

    update

--- App/main.js
+++ App/main.js
@@ -3,10 +3,10 @@ import koa from 'koa';
 class APP {
 	constructor(props) {
 		this.app = new koa();
-	},
+	}
 	init() {
 
-	},
+	}
 	start(port) {
 		let app = this.app;
 		app.use((ctx)=>{
@@ -18,6 +18,7 @@ class APP {
 
 const project = new APP();
 console.log('project测试', project)
+project.start(3000);
 
-// export default project;
-module.exports = project;
+export default project;
+// module.exports = project;
