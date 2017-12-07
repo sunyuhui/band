c02e3bca6fb9d3b3772d7de7e5666bb7945a73b0
sunhui04 <sunhui04@meituan.com>
2017-12-07 20:35:37 +0800

    add koa-router

--- App/index.js
+++ App/index.js
@@ -1,15 +1,17 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 class APP {
 	constructor(props) {
 		this.app = new Koa();
+		this.router = new Router();
 	}
 	init() {
 
 	}
 	start(port) {
 		let app = this.app;
-		
+		let router = this.router;
 		app.listen(port);
 	}
 }
